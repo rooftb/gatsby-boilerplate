d75f0b90e96247ba8a118646e30d79fd68016c79
Tanner <rooftb@email.sc.edu>
2020-04-21 11:49:32 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/header"
+import Footer from "../components/footer"
 import "../styles/index.scss"
 import layoutStyles from "./layout.module.scss"
 
